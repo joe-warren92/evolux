9884a952bfdda9be6b1872d307dd056bd67e1ca6
joe-warren92 <joe@evoluxwebdesign.co.uk>
2026-06-02 13:53:28 +0100

    added sitemap and robots.txt

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,61 @@
+import type { APIRoute } from "astro";
+import { INSIGHT_POSTS } from "../data/insights";
+import { LOCATION_PAGES } from "../data/locations";
+import { PROJECTS } from "../data/projects";
+import { SERVICES } from "../data/services";
+
+const SITE_URL = "https://evoluxwebdesign.co.uk";
+
+interface SitemapEntry {
+  path: string;
+  lastModified?: string;
+}
+
+const staticEntries: SitemapEntry[] = [
+  { path: "" },
+  { path: "/about" },
+  { path: "/contact" },
+  { path: "/free-review" },
+  { path: "/packages" },
+  { path: "/services" },
+  { path: "/projects" },
+  { path: "/insights" },
+  { path: "/locations" }
+];
+
+const entries: SitemapEntry[] = [
+  ...staticEntries,
+  ...SERVICES.map(({ slug }) => ({ path: `/services/${slug}` })),
+  ...PROJECTS.map(({ slug }) => ({ path: `/projects/${slug}` })),
+  ...LOCATION_PAGES.map(({ slug }) => ({ path: `/locations/${slug}` })),
+  ...INSIGHT_POSTS.map(({ slug, updatedAt, publishedAt }) => ({
+    path: `/insights/${slug}`,
+    lastModified: updatedAt ?? publishedAt
+  }))
+];
+
+const escapeXml = (value: string) =>
+  value
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;");
+
+export const GET: APIRoute = () => {
+  const urls = entries
+    .map(({ path, lastModified }) => {
+      const lastModifiedElement = lastModified ? `<lastmod>${escapeXml(lastModified)}</lastmod>` : "";
+      return `<url><loc>${escapeXml(`${SITE_URL}${path}`)}</loc>${lastModifiedElement}</url>`;
+    })
+    .join("");
+
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>`,
+    {
+      headers: {
+        "Content-Type": "application/xml; charset=utf-8"
+      }
+    }
+  );
+};
